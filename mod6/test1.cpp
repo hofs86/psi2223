091363457fce2d972fc6e0bbfc441106dc5fb82b
Sergio Henriques <sergio.henriques@aebatalha.ccems.pt>
2023-05-30 16:08:03 +0100

    df

--- mod6/test1.cpp
+++ mod6/test1.cpp
@@ -4,11 +4,13 @@ using namespace std;
 
 int main(){
     setlocale(LC_ALL, "pt_PT.utf8");
-    int n;
+    // int n;
 
-    cin >> n;
+    // cin >> n;
 
-    int notas[n];
+    // int notas[3];
+
+    cout << "Hello teste!";
     
     return 0;
 }
