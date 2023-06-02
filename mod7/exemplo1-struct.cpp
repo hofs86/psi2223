30b97a27e3d5d4d004f3c4236ddba9d6e027c50d
Sergio Henriques <sergio.henriques@aebatalha.ccems.pt>
2023-06-02 10:40:48 +0100

    aulas 2

--- mod7/exemplo1-struct.cpp
+++ mod7/exemplo1-struct.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 using namespace std;
 
 struct Carro
@@ -22,7 +23,7 @@ void adicionar_carro()
 
     cout << "introduza o modelo: ";
     getline(cin, item.modelo);
- 
+
     cout << "introduza o ano: ";
     cin.ignore();
     cin >> item.ano;
@@ -33,26 +34,58 @@ void adicionar_carro()
     fout << item.marca << ';' << item.modelo << ';' << item.ano << "\n";
     fout.close();
 
-    cout << "Original: " << item.marca << ' ' << item.modelo << ' ' << item.ano << endl;
+    cout << "Original: " << item.marca << ' ' << item.modelo << ' ' << item.ano << endl
+         << endl;
 }
 
 void ler_ficheiro()
 {
     Carro from_file;
+    string linhaLida;
 
     // Abrir ficheiro e ler carro do ficheiro para dentro da struct
     cout << "Reading Carro from file" << endl;
     ifstream fin("mydata.txt");
-    fin >> from_file.marca >> from_file.modelo >> from_file.ano;
+    while (getline(fin, linhaLida))
+    {
+        stringstream linha(linhaLida);
+        // string atributo;
+        // cout << "Carro\n";
+        // while (getline(linha, atributo, ';'))
+        // {
+        //     cout << "\tMarca: " << atributo << "\n";
 
-    cout << "Read in:  " << from_file.marca << ' ' << from_file.modelo << ' ' << from_file.ano << endl;
+        //     getline(linha, atributo, ';');
+        //     cout << "\tModelo: " << atributo << "\n";
+
+        //     getline(linha, atributo, ';');
+        //     cout << "\tAno: " << atributo << "\n\n";
+        // }
+
+        linha >> from_file.marca >> from_file.modelo >> from_file.ano;
+
+        cout << "Carro\n";
+        cout << "\tMarca: " << from_file.marca << "\n";
+        cout << "\tModelo: " << from_file.modelo << "\n";
+        cout << "\tAno: " << from_file.ano << "\n\n";
+        // cout << from_file.marca << ' ' << from_file.modelo << ' ' << from_file.ano << endl;
+    }
 
     fin.close();
 }
 
+void apagar_e_acrescentar_um_carro()
+{
+    // abrir ficheiro e limpar
+    ofstream fout("mydata.txt", ios::trunc);
+    fout.close();
+    // chamar funcao para adicionar outro carro
+    adicionar_carro();
+}
+
 int main()
 {
-    adicionar_carro();
+    // adicionar_carro();
 
     ler_ficheiro();
 
